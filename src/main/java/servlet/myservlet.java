50b025c70600c95324293bf1ae62946fc2e0581b
2014WRX <425303767@qq.com>
2017-07-07 14:18:46 +0800

    update

---
+++ src/main/java/servlet/myservlet.java
@@ -0,0 +1,59 @@
+package servlet;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class myservlet extends HttpServlet {
+
+	@Override
+	protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doDelete(req, resp);
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		resp.setContentType("UTF-8");
+		PrintWriter out = resp.getWriter();
+		String username = req.getParameter("username");
+		String psw = req.getParameter("psw");
+		//print jsom //print html 
+		if(username.equals("12345")&&psw.equals("123456"))
+		{
+			out.println("Welcome "+username);
+			out.flush();
+			out.close();
+		}
+		else 
+		{
+			out.println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">");
+			out.println("<HTML>");
+			out.println("  <HEAD><TITLE>A Servlet</TITLE></HEAD>");
+			out.println("  <BODY>");
+			out.print("    error: your username "+ username+" or password is wrong"+psw);
+			out.println("  </BODY>");
+			out.println("</HTML>");
+			out.flush();
+			out.close();
+		}
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPost(req, resp);
+	}
+
+	@Override
+	protected void service(HttpServletRequest arg0, HttpServletResponse arg1) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.service(arg0, arg1);
+	}
+	
+	
+}
