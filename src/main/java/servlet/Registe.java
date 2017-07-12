524f82951b4cb2b3b5695300ee1db03ded1be98b
2014WRX <425303767@qq.com>
2017-07-12 11:56:52 +0800

    update

--- src/main/java/servlet/Registe.java
+++ src/main/java/servlet/Registe.java
@@ -4,7 +4,11 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.ParsePosition;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Properties;
 
 import javax.servlet.ServletException;
@@ -83,16 +87,17 @@ public class Registe extends HttpServlet {
 		String num = request.getParameter("num");
 		String where="";
 		
-		int nums=0;
+		int newnum=0;
 		for(int i=0;i<num.length();i++)
-			nums = nums*10+(num.charAt(i)-'0');
+			newnum = newnum*10+(num.charAt(i)-'0');
 		year=year.replace("-", "");
 		if(whereid.equals("0"))
 			where="计算机学院";
 		if(whereid.equals("1"))
 			where="其他学院";
 		
-		for(int i=0;i<nums;i++)
+		
+		for(int i=0;i<newnum;i++)
 		{
 			String stunum = year+whereid+""+(i+1);
 			JSONObject StuNo =new JSONObject();
@@ -115,54 +120,78 @@ public class Registe extends HttpServlet {
 
 	public boolean createtable(){
 		
+		Connection conn = null; 
+		Statement  s =null;
 		try{
 		Class.forName("org.apache.derby.jdbc.EmbeddedDriver").newInstance(); 
-		System.out.println("Load the embedded driver"); 
-		Connection conn = null; 
+	
 		Properties props = new Properties(); 
 		//create and connect the database named helloDB 
 		conn=DriverManager.getConnection("jdbc:derby:wust5DB;create=true", props);  
 		conn.setAutoCommit(false); 
 		
-		// create a table and insert two records 
-		Statement s = conn.createStatement(); 
-		s.execute("drop table if exists 'testtable'");
-		s.execute("create table testtable(place varchar(40), StuNo char(10))");
-		
+		s = conn.createStatement(); 
+		s.execute("drop table testtable");
+		s.execute("create table testtable(place varchar(40), StuNo char(10) ,Psw char(20))");
+		System.out.println("finish create table"); 
+		s.close();
 		return true;
 		}catch(Exception e){
 			return false;
+		}finally{
+			if(null != s)
+			{
+				try {
+					s.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
 		}
+		
 	}
 	
 	public boolean save(String where,String StuNo)throws ServletException, IOException {
 		//This code uses for saving numbers informations
 		
-		String message="insert into testtable values";
+		System.out.print("start saving !");
 		
+		Connection conn = null; 
+		Statement s = null;
 		try { // load the driver 
 			Class.forName("org.apache.derby.jdbc.EmbeddedDriver").newInstance(); 
-			Connection conn = null; 
 			Properties props = new Properties(); 
-			//create and connect the database named helloDB 
-			conn=DriverManager.getConnection("jdbc:derby:wust5DB;create=true", props); 
-			conn.setAutoCommit(false); 
+			//create and connect the database named wust5DB 
+			conn=DriverManager.getConnection("jdbc:derby:wust5DB;create=true", props);  
 			
-			
-			// create a table and insert two records 
-			Statement s = conn.createStatement(); 
-		//	s.execute("create table testtable(place varchar(40), StuNo char(10))");
-			s.execute("insert into testtable values('计算机学院', '2014101901')"); 
-			//s.execute(message+" ('"+where+"','"+StuNo+"')");
+			//insert records 
+			s = conn.createStatement(); 
+			s.execute("insert into testtable values('计算机学院','2014101901','2014101901')"); 
 			 
-			s.close();  
+			//s.close();  
 			conn.commit(); 
-			conn.close(); 
+			//conn.close(); 
 			return true;
 			
 		}catch (Exception e){
 			e.printStackTrace();
 			return false;
+		}finally{
+			if(null != s){
+				try {
+					s.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if(null != conn){
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
 		}
 	}
 	
--- src/main/java/servlet/myservlet.java
+++ src/main/java/servlet/myservlet.java
@@ -1,6 +1,10 @@
 package servlet;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.util.Properties;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -9,6 +13,14 @@ import javax.servlet.http.HttpServletResponse;
 
 public class myservlet extends HttpServlet {
 
+	@Override
+	protected void service(HttpServletRequest arg0, HttpServletResponse arg1) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.service(arg0, arg1);
+		if(connDB()==false)
+			destroy();
+	}
+	
 	@Override
 	protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
@@ -49,13 +61,78 @@ public class myservlet extends HttpServlet {
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
 		
+		resp.setContentType("text/html");
+		resp.setCharacterEncoding("UTF-8");
+		PrintWriter out = resp.getWriter();
+		String username = req.getParameter("username");
+		String psw = req.getParameter("psw");
+		
+		if(checkmessage(username,psw))
+			out.println("登录成功");
+		else 
+			out.println(" error: your print username or password is wrong !");
 	}
 
 	@Override
-	protected void service(HttpServletRequest arg0, HttpServletResponse arg1) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		super.service(arg0, arg1);
+	
+	public void destroy() {
+		super.destroy(); // Just puts "destroy" string in log
+		// Put your code here
 	}
 	
+	public boolean connDB(){
+		
+		try{
+			Class.forName("org.apache.derby.jdbc.EmbeddedDriver").newInstance(); 
+			System.out.println("Load the embedded driver"); 
+			Connection conn = null; 
+			Properties props = new Properties(); 
+			//create and connect the database named helloDB 
+			conn=DriverManager.getConnection("jdbc:derby:wust5DB;create=true", props);  
+			conn.setAutoCommit(false); 
+			
+			// create a table and insert two records 
+			Statement s = conn.createStatement(); 
+			s.execute("drop table testtable");
+			s.execute("create table testtable(place varchar(40), StuNo char(10),Psw char(20))");
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return true;
+	}
+	
+	public boolean checkmessage(String username,String password){
+		
+		
+		String message="select * from testtable where StuNo='"+username+"' and Psw='"+password+"'";
+		try { // load the driver 
+			Class.forName("org.apache.derby.jdbc.EmbeddedDriver").newInstance(); 
+			Connection conn = null; 
+			Properties props = new Properties(); 
+			//connect
+			conn=DriverManager.getConnection("jdbc:derby:wust5DB;create=true", props); 
+			conn.setAutoCommit(false); 
+			
+			
+			// select message
+			Statement s = conn.createStatement(); 
+			
+			if(s.execute(message))
+			{
+				s.close();  
+				conn.commit(); 
+				conn.close(); 
+				return true;
+			}
+			else {
+				return false;
+			}
+			
+		}catch (Exception e){
+			e.printStackTrace();
+			return false;
+		}
+		
+	}
 	
 }
